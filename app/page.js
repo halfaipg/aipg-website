ba7b2ac2a6ba16d8af5aba76bf007ea92ac38a18
websines <to.subh03@gmail.com>
2023-11-19 23:27:39 +0530

    change-2

--- app/page.js
+++ app/page.js
@@ -1,12 +1,28 @@
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Services from "@/components/Services";
+import Stats from "@/components/Stats";
 import TopBar from "@/components/TopBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <TopBar />
-      <Navbar />
+    <main className="">
+      <Hero />
+      <Stats />
+      <div className="flex flex-1 self-center w-full justify-center items-center bg-gray-50">
+        <span className="text-2xl sm:text-4xl font-semibold py-4 sm:py-8">
+          Our Services
+        </span>
+      </div>
+      <Services />
+      <div className="flex flex-1 self-center w-full justify-center items-center bg-gray-50">
+        <span className="text-2xl sm:text-4xl font-semibold py-4 sm:py-8">
+          Features
+        </span>
+      </div>
+      <Features />
     </main>
   );
 }
