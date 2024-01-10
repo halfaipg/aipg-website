2faeba2ce177ec797a373b8efb1436a2dbf1fce7
root <root@vultr.guest>
2024-01-10 05:00:09 +0000

    Added tokenomics page, changed size of logos on wallets page.

--- app/tokenomics/page.js
+++ app/tokenomics/page.js
@@ -1,27 +1,74 @@
-import React from "react";
+"use client";
+
+import React, { useState } from 'react';
 
 const Page = () => {
+  const totalPages = 5; // Total number of tokenomics explanation images
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const nextPage = () => {
+    setCurrentPage((prevPage) => (prevPage + 1) % totalPages);
+  };
+
+  const prevPage = () => {
+    setCurrentPage((prevPage) => (prevPage - 1 + totalPages) % totalPages);
+  };
+
+  const tokenomicsImages = Array.from({ length: totalPages }, (_, i) => 
+    `/tokenomics-images/AIPG_Tokenomics-${i + 1}.png`
+  );
+
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="flex-grow p-4 mx-auto text-center">
+      <main className="flex-grow text-center">
         <h1 className="text-4xl mt-8 mb-12">Tokenomics</h1>
-        <h2 className="text-xl font-bold mb-2">Distribution over time</h2>
-        <img src="/Tokenomics_Graph.png" alt="Tokenomics Graph" className="w-3/4 mx-auto" />
-        <section>
-          {/* Add the content for the "Distribution over time" section here */}
+        <h2 className="text-xl font-bold mb-2">Distribution Over Time</h2>
+        <img src="/Tokenomics_Graph.png" alt="Tokenomics Graph" className="w-full sm:w-1/2 mx-auto" />
+        <section className="pt-16 pb-16">
+          <h2 className="text-xl font-bold mb-2">Explanation</h2>
+          {/* Image gallery for mobile */}
+          <div className="sm:hidden flex flex-col items-center">
+            <img
+              src={tokenomicsImages[currentPage]}
+              alt={`Tokenomics Explanation Page ${currentPage + 1}`}
+              className="mb-4"
+            />
+            <div className="flex justify-between w-full px-0">
+              <button
+                onClick={prevPage}
+                className="w-32 px-4 py-1 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+                disabled={currentPage === 0}
+              >
+                Previous Page
+              </button>
+              <button
+                onClick={nextPage}
+                className="w-32 px-4 py-1 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+                disabled={currentPage === totalPages - 1}
+              >
+                Next<br />Page
+              </button>
+            </div>
+            {/* Download button for mobile */}
+            <a
+              href="/AIPG_Tokenomics.pdf"
+              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+              download
+            >
+              Download Tokenomics
+            </a>
+          </div>
+          {/* PDF Embed for larger screens */}
+          <div className="hidden sm:block text-center">
+            <embed src="/AIPG_Tokenomics.pdf" type="application/pdf" className="mx-auto" style={{ width: '54%', height: '950px' }} />
+          </div>
         </section>
-        <section>
-          <h2 className="text-xl font-bold mb-2">Find a good distribution</h2>
-          {/* Add the content for the "Find a good distribution" section here */}
-        </section>
-        <section>
-          <h2 className="text-xl font-bold mb-2">Microhalving</h2>
-          {/* Add the content for the "Microhalving" section here */}
-        </section>
-        <section>
-          <h2 className="text-xl font-bold mb-2">Distribution Table</h2>
-          {/* Add the distribution tables here */}
-          <img src="/Tokenomics_Chart.png" alt="Tokenomics Chart" className="w-3/4 mx-auto" />
+        <section className="pt-16 pb-16">
+          <div className="">
+            <h2 className="text-xl font-bold mb-2">Distribution Table</h2>
+            {/* Add the distribution tables here */}
+            <img src="/Tokenomics_Chart.png" alt="Tokenomics Chart" className="w-full md:w-3/4 lg:w-2/3 mx-auto"/>
+          </div>
         </section>
       </main>
     </div>
