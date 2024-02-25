6534cf4618448d1b91b2e09bd7fde9c4ac99874b
Mandark <hanswashed151@gmail.com>
2024-02-25 22:20:56 +0000

    Updated wallet to include webwallet and added Telegram button to main page.

--- app/page.js
+++ app/page.js
@@ -1,36 +1,37 @@
-import BlogsSection from "@/components/BlogsSection";
-import FAQ from "@/components/FAQ";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import MiningPoolSection from "@/components/MiningPoolSection";
 import Services from "@/components/Services";
-import Stats from "@/components/Stats";
+// import Team from "@/components/Team"; // Re-added Team import
 import Timeline from "@/components/Timeline";
 
 export default function Home() {
   return (
     <main className="dark">
       <Hero />
-      {/* </Stats /> */}
       <div className="flex flex-1 self-center w-full justify-center items-center bg-gray-50 dark:bg-black">
         <span className="text-2xl sm:text-4xl font-semibold py-4 sm:py-8">
           Features
         </span>
       </div>
-      <Services />
+      <Features />
+      {/* <div className="flex flex-1 self-center w-full justify-center items-center bg-gray-50 dark:bg-black">
+        <span className="text-2xl sm:text-4xl font-semibold py-4 sm:py-8">
+          The Team
+        </span>
+      </div>
+      <Team /> */}
       <div className="flex flex-1 self-center w-full justify-center items-center bg-gray-50 dark:bg-black">
         <span className="text-2xl sm:text-4xl font-semibold py-4 sm:py-8">
           Our Vision
         </span>
       </div>
-      <Features />
+      <Services />
       <div className="flex flex-1 self-center w-full justify-center items-center bg-gray-50 dark:bg-black">
         <span className="text-2xl sm:text-4xl font-semibold py-4 sm:py-8">
           Roadmap
         </span>
       </div>
       <Timeline />
-      {/* <BlogsSection /> */}
     </main>
   );
 }
