026d3f62a126b043bf0a5a5ee986488273dd8491
j <admin@aipowergrid.io>
2025-03-25 20:00:09 -0400

    Replaced all instances of class with className Converted SVG attribute names from kebab-case to camelCase: stroke-width to strokeWidth stroke-linecap to strokeLinecap stroke-linejoin to strokeLinejoin

--- app/about/page.js
+++ app/about/page.js
@@ -52,7 +52,7 @@ const About = () => {
   };
 
   return (
-    <div className="pt-10">
+    <div className="pt-0">
       {/* Video Banner */}
       <div className="overflow-hidden">
         <video autoPlay loop muted className="w-full h-[190px] sm:h-[300px] md:h-[350px] lg:h-[400px] object-cover">
@@ -61,13 +61,13 @@ const About = () => {
         </video>
       </div>
 
-      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-        <div class="mt-5 lg:mt-16 grid lg:grid-cols-3 gap-8 lg:gap-12">
-          <div class="lg:col-span-1">
-            <h2 class="font-bold text-2xl md:text-3xl text-gray-800 dark:text-gray-200">
+      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className="mt-5 lg:mt-16 grid lg:grid-cols-3 gap-8 lg:gap-12">
+          <div className="lg:col-span-1">
+            <h2 className="font-bold text-2xl md:text-3xl text-gray-800 dark:text-gray-200">
               What is AI Power Grid?
             </h2>
-            <p class="mt-2 md:mt-4 text-gray-500">
+            <p className="mt-2 md:mt-4 text-gray-500">
               AI Power Grid (AIPG) is pioneering a revolutionary approach by
               offering free access to cutting-edge Large Language Model (LLM)
               inference and AI Art generation, validated on an immutable
@@ -83,73 +83,73 @@ const About = () => {
             </p>
           </div>
 
-          <div class="lg:col-span-2">
-            <div class="grid sm:grid-cols-2 gap-8 md:gap-12">
-            <div class="flex gap-x-5">
+          <div className="lg:col-span-2">
+            <div className="grid sm:grid-cols-2 gap-8 md:gap-12">
+            <div className="flex gap-x-5">
                 <svg
-                  class="flex-shrink-0 mt-1 w-6 h-6 text-blue-600 dark:text-blue-500"
+                  className="flex-shrink-0 mt-1 w-6 h-6 text-blue-600 dark:text-blue-500"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
                   <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
                 </svg>
-                <div class="grow">
-                  <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+                <div className="grow">
+                  <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
                   AI Democratization
                   </h3>
-                  <p class="mt-1 text-gray-600 dark:text-gray-400">
+                  <p className="mt-1 text-gray-600 dark:text-gray-400">
                   The platform aims to make AI accessible to all by lowering the barriers to entry. 
                   This democratization allows contributors from various backgrounds to participate 
                   equally in the future of AI development and application, aligning with the open-source and democratizing philosophy of AIPG.
                   </p>
                 </div>
               </div>
-              <div class="flex gap-x-5">
+              <div className="flex gap-x-5">
                 <svg
-                  class="flex-shrink-0 mt-1 w-6 h-6 text-blue-600 dark:text-blue-500"
+                  className="flex-shrink-0 mt-1 w-6 h-6 text-blue-600 dark:text-blue-500"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path d="M7 10v12" />
                   <path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z" />
                 </svg>
-                <div class="grow">
-                  <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+                <div className="grow">
+                  <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
                     Decentralization and Accessibility
                   </h3>
-                  <p class="mt-1 text-gray-600 dark:text-gray-400">
+                  <p className="mt-1 text-gray-600 dark:text-gray-400">
                     One of AIPG's core values is the democratization of access to AI. The project aims to make AI technologies widely accessible and equitable, 
                     empowering users across different sectors to harness AI's capabilities for innovation and growth.
                   </p>
                 </div>
               </div>
-              <div class="flex gap-x-5">
+              <div className="flex gap-x-5">
                 <svg
-                  class="flex-shrink-0 mt-1 w-6 h-6 text-blue-600 dark:text-blue-500"
+                  className="flex-shrink-0 mt-1 w-6 h-6 text-blue-600 dark:text-blue-500"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <rect width="18" height="10" x="3" y="11" rx="2" />
                   <circle cx="12" cy="5" r="2" />
@@ -157,11 +157,11 @@ const About = () => {
                   <line x1="8" x2="8" y1="16" y2="16" />
                   <line x1="16" x2="16" y1="16" y2="16" />
                 </svg>
-                <div class="grow">
-                  <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+                <div className="grow">
+                  <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
                     ASIC resistant algo
                   </h3>
-                  <p class="mt-1 text-gray-600 dark:text-gray-400">
+                  <p className="mt-1 text-gray-600 dark:text-gray-400">
                   AIPG uses the KAWPOW hashing algorithm to discourage the
                     use of ASIC hardware. ASIC resistance is vital for ensuring that no 
                     single participant or group can monopolize the mining process, and 
@@ -169,29 +169,29 @@ const About = () => {
                   </p>
                 </div>
               </div>
-              <div class="flex gap-x-5">
+              <div className="flex gap-x-5">
                 <svg
-                  class="flex-shrink-0 mt-1 w-6 h-6 text-blue-600 dark:text-blue-500"
+                  className="flex-shrink-0 mt-1 w-6 h-6 text-blue-600 dark:text-blue-500"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
                   <circle cx="9" cy="7" r="4" />
                   <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
                   <path d="M16 3.13a4 4 0 0 1 0 7.75" />
                 </svg>
-                <div class="grow">
-                  <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+                <div className="grow">
+                  <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
                     Community and Collaboration
                   </h3>
-                  <p class="mt-1 text-gray-600 dark:text-gray-400">
+                  <p className="mt-1 text-gray-600 dark:text-gray-400">
                   AIPG is dedicated to fostering an active, engaging community and establishing partnerships with entities in the open-source AI space and related industries. This collaborative approach is intended to 
                   ensure AIPG remains at the cutting edge of AI and blockchain technology integration.
                   </p>
@@ -302,16 +302,16 @@ const About = () => {
         </div>
       </div>
 
-      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-        <div class="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
-          <div class="mt-5 sm:mt-10 lg:mt-0">
-            <div class="space-y-6 sm:space-y-8">
-              <div class="space-y-2 md:space-y-4">
-                <h2 class="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-gray-200">
+      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
+          <div className="mt-5 sm:mt-10 lg:mt-0">
+            <div className="space-y-6 sm:space-y-8">
+              <div className="space-y-2 md:space-y-4">
+                <h2 className="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-gray-200">
                   The AIPG coin is the native cryptocurrency of the AI Power
                   Grid platform.
                 </h2>
-                <p class="text-gray-500">
+                <p className="text-gray-500">
                   Our ecosystem will thrive on open-source collaboration, fostering
                   innovation and creativity, while our marketplace and NFT AI
                   Gallery enable artists and AI enthusiasts to monetize their
@@ -324,101 +324,101 @@ const About = () => {
                   high-quality artworks that are tokenized as NFTs on the blockchain.
                 </p>
               </div>
-              <ul role="list" class="space-y-2 sm:space-y-4">
-                <li class="flex space-x-3">
-                  <span class="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+              <ul role="list" className="space-y-2 sm:space-y-4">
+                <li className="flex space-x-3">
+                  <span className="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
                     <svg
-                      class="flex-shrink-0 h-3.5 w-3.5"
+                      className="flex-shrink-0 h-3.5 w-3.5"
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
                       height="24"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
                       <polyline points="20 6 9 17 4 12" />
                     </svg>
                   </span>
 
-                  <span class="text-sm sm:text-base text-gray-500">
-                    <span class="font-bold">Decentralized</span> AI services
+                  <span className="text-sm sm:text-base text-gray-500">
+                    <span className="font-bold">Decentralized</span> AI services
                   </span>
                 </li>
 
-                <li class="flex space-x-3">
-                  <span class="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                <li className="flex space-x-3">
+                  <span className="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
                     <svg
-                      class="flex-shrink-0 h-3.5 w-3.5"
+                      className="flex-shrink-0 h-3.5 w-3.5"
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
                       height="24"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
                       <polyline points="20 6 9 17 4 12" />
                     </svg>
                   </span>
 
-                  <span class="text-sm sm:text-base text-gray-500">
-                    NFT <span class="font-bold">Art Gallery</span>
+                  <span className="text-sm sm:text-base text-gray-500">
+                    NFT <span className="font-bold">Art Gallery</span>
                   </span>
                 </li>
 
-                <li class="flex space-x-3">
-                  <span class="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                <li className="flex space-x-3">
+                  <span className="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
                     <svg
-                      class="flex-shrink-0 h-3.5 w-3.5"
+                      className="flex-shrink-0 h-3.5 w-3.5"
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
                       height="24"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
                       <polyline points="20 6 9 17 4 12" />
                     </svg>
                   </span>
 
-                  <span class="text-sm sm:text-base text-gray-500">
+                  <span className="text-sm sm:text-base text-gray-500">
                     Blockchain-Validated Outputs
                   </span>
                 </li>
-                <li class="flex space-x-3">
-                  <span class="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                <li className="flex space-x-3">
+                  <span className="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
                     <svg
-                      class="flex-shrink-0 h-3.5 w-3.5"
+                      className="flex-shrink-0 h-3.5 w-3.5"
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
                       height="24"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
                       <polyline points="20 6 9 17 4 12" />
                     </svg>
                   </span>
 
-                  <span class="text-sm sm:text-base text-gray-500">
+                  <span className="text-sm sm:text-base text-gray-500">
                     Rapid Deployment and Community Feedback Loop
                   </span>
                 </li>
               </ul>
             </div>
           </div>
-          <div class={`${isLoading ? "" : "mt-6 sm:mt-0"}`}>
+          <div className={`${isLoading ? "" : "mt-6 sm:mt-0"}`}>
             {isLoading ? (
               <div>Loading...</div> // replace this with your loading spinner or placeholder
             ) : (
